22386f8984622d884d7098c75e15ff0d84fac731
LittleCadet <sx255879@163.com>
2019-02-26 00:11:11 +0800

    测试html

--- src/main/java/com/test/controller/ZkController.java
+++ src/main/java/com/test/controller/ZkController.java
@@ -1,6 +1,5 @@
-package com.test;
+package com.test.controller;
 
-import com.application.RegisterApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ZkController
 {
-    private static final Logger logger = LoggerFactory.getLogger(RegisterApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(ZkController.class);
 
     @Value("${zookeeper.zkserver}")
     public String zkserver;
